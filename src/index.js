356c2498e8bb0a49fea87f7fb2a63a7f0ac57445
Marcus <marcus.gamboa@gmail.com>
2021-02-18 13:28:52 -0700

    Initialized

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class App extends React.Component {
+  render() {
+    const style = {
+      base: "flex",
+      h1: "text-4xl w-full text-center",
+    };
+    return (
+      <div className={style.base}>
+        <h1 className={style.h1}>Hello World H1</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
